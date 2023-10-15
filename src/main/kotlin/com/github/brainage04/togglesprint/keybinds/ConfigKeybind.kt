2597d5ce36206ce3e436433628fe49fb8d9dcb26
brainage04 <thomasmcmah21@gmail.com>
2023-10-16 02:07:29 +1000

    fixed config key

--- src/main/kotlin/com/github/brainage04/togglesprint/keybinds/ConfigKeybind.kt
+++ src/main/kotlin/com/github/brainage04/togglesprint/keybinds/ConfigKeybind.kt
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.lwjgl.input.Keyboard
 
-class ConfigKeybind : KeyBinding("Open Config Menu", Keyboard.KEY_NUMPADENTER, ToggleSprintMain.MOD_NAME) {
+class ConfigKeybind : KeyBinding("Open Config Menu", Keyboard.KEY_RETURN, ToggleSprintMain.MOD_NAME) {
     @SubscribeEvent
     fun tick(event: TickEvent.ClientTickEvent) {
         if (event.phase == TickEvent.Phase.START) {
