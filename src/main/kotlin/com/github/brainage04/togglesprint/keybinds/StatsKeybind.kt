676158c7c9882d835673941ff7cf29a1729e6986
brainage04 <thomasmcmah21@gmail.com>
2024-01-25 04:34:49 +1000

    stats keybind changes

--- src/main/kotlin/com/github/brainage04/togglesprint/keybinds/StatsKeybind.kt
+++ src/main/kotlin/com/github/brainage04/togglesprint/keybinds/StatsKeybind.kt
@@ -4,6 +4,7 @@ import com.github.brainage04.togglesprint.ToggleSprintMain
 import com.github.brainage04.togglesprint.utils.ChatUtils
 import net.minecraft.client.Minecraft
 import net.minecraft.client.settings.KeyBinding
+import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.lwjgl.input.Keyboard
@@ -12,10 +13,16 @@ class StatsKeybind : KeyBinding("Stats Keybind", Keyboard.KEY_NONE, ToggleSprint
     @SubscribeEvent
     fun tick(event: TickEvent.ClientTickEvent) {
         if (event.phase == TickEvent.Phase.START) {
-            if (Minecraft.getMinecraft().thePlayer == null) return
+            val player = Minecraft.getMinecraft().thePlayer ?: return
 
             if (this.isPressed) {
-                ChatUtils.messageToChat("testing", soundType = ChatUtils.SoundType.NOTIFICATION)
+                val item = player.heldItem ?: return
+
+                var string = item.item.getItemStackDisplayName(ItemStack(item.item))
+
+                if (item.hasDisplayName()) string = item.displayName
+
+                ChatUtils.messageToChat(string, soundType = ChatUtils.SoundType.NOTIFICATION)
             }
         }
     }
