b2a6b2ebbd2000057a6980620fdb77e028c9fcce
brainage04 <thomasmcmah21@gmail.com>
2024-01-25 01:50:15 +1000

    added C/E counters to PlayerPositionTracker

--- src/main/kotlin/com/github/brainage04/togglesprint/gui/PlayerPositionTracker.kt
+++ src/main/kotlin/com/github/brainage04/togglesprint/gui/PlayerPositionTracker.kt
@@ -2,6 +2,7 @@ package com.github.brainage04.togglesprint.gui
 
 import com.github.brainage04.togglesprint.ToggleSprintMain
 import com.github.brainage04.togglesprint.gui.core.RenderGuiData
+import com.github.brainage04.togglesprint.utils.ChatUtils
 import com.github.brainage04.togglesprint.utils.MathUtils.round
 import net.minecraft.client.Minecraft
 
@@ -12,10 +13,16 @@ object PlayerPositionTracker {
         if (!guiElements.positionTracker.coreSettings.isEnabled) return
 
         val textArray = arrayListOf(
-            "§fX: ${Minecraft.getMinecraft().thePlayer.posX.round(guiElements.positionTracker.decimals)}",
-            "§fY: ${Minecraft.getMinecraft().thePlayer.posY.round(guiElements.positionTracker.decimals)}",
-            "§fZ: ${Minecraft.getMinecraft().thePlayer.posZ.round(guiElements.positionTracker.decimals)}",
+            "${ChatUtils.whiteChar}X: ${Minecraft.getMinecraft().thePlayer.posX.round(guiElements.positionTracker.decimals)}",
+            "${ChatUtils.whiteChar}Y: ${Minecraft.getMinecraft().thePlayer.posY.round(guiElements.positionTracker.decimals)}",
+            "${ChatUtils.whiteChar}Z: ${Minecraft.getMinecraft().thePlayer.posZ.round(guiElements.positionTracker.decimals)}",
         )
+
+        if (guiElements.positionTracker.showChunkCounter) {
+            val text = ChatUtils.whiteChar + Minecraft.getMinecraft().renderGlobal.debugInfoRenders.split(" ")
+            textArray.add("${ChatUtils.whiteChar + text[0]} ${text[1]}")
+        }
+        if (guiElements.positionTracker.showEntityCounter) textArray.add(Minecraft.getMinecraft().renderGlobal.debugInfoEntities.split(",")[0])
 
         RenderGuiData.renderElement(
             guiElements.positionTracker.coreSettings.x,
