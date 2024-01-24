4c90870ef34feff44ed5d1e5ba30645a75581533
brainage04 <thomasmcmah21@gmail.com>
2024-01-25 01:15:20 +1000

    added all chat char types

--- src/main/kotlin/com/github/brainage04/togglesprint/utils/ChatUtils.kt
+++ src/main/kotlin/com/github/brainage04/togglesprint/utils/ChatUtils.kt
@@ -7,10 +7,35 @@ import net.minecraft.client.Minecraft
 import net.minecraft.util.ChatComponentText
 
 object ChatUtils {
-    private const val defaultPrefix = "§e[${MOD_NAME}]§r "
-    private const val redPrefix = "§e[${MOD_NAME}]§c "
-    private const val yellowPrefix = "§e[${MOD_NAME}]§e "
-    private const val greenPrefix = "§e[${MOD_NAME}]§a "
+    const val darkRedChar = "§4"
+    const val redChar = "§c"
+    const val goldChar = "§6"
+    const val yellowChar = "§e"
+    const val darkGreenChar = "§2"
+    const val greenChar = "§a"
+    const val aquaChar = "§b"
+    const val darkAquaChar = "§3"
+    const val darkBlueChar = "§1"
+    const val blueChar = "§9"
+    const val lightPurpleChar = "§d"
+    const val darkPurpleChar = "§5"
+    const val whiteChar = "§f"
+    const val grayChar = "§7"
+    const val darkGrayChar = "§8"
+    const val blackChar = "§0"
+
+    const val obfuscatedChar = "§k"
+    const val boldChar = "§l"
+    const val strikethroughChar = "§m"
+    const val underlineChar = "§n"
+    const val italicChar = "§o"
+
+    const val resetChar = "§r"
+
+    private const val defaultPrefix = "$yellowChar[${MOD_NAME}]$resetChar "
+    private const val redPrefix = "$yellowChar[${MOD_NAME}]$redChar "
+    private const val yellowPrefix = "$yellowChar[${MOD_NAME}]$yellowChar "
+    private const val greenPrefix = "$yellowChar[${MOD_NAME}]$greenChar "
 
     enum class PrefixType {
         DEFAULT,
